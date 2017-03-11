aa5cdfb25d36df10135fb85c84c7e83ff4655bed
Nicole Ferraro <nicoleferraro@Nicoles-MacBook-Pro-3.local>
2017-03-11 13:19:54 -0800

    Include training for validation step

--- fit_enet_bayes_models.R
+++ fit_enet_bayes_models.R
@@ -8,11 +8,18 @@ options(warn=-1)
 
 #Set personal working directory
 setwd('/Users/nicoleferraro/Documents/Stanford/Winter_1617/BMI217/Project/PredictionAddiction/')
+#Read in genes from validation set
+gene_list = readRDS('TCGA-Gene-Set')
 
 #Assumes data files are stored in CCLE folder, one directory up, change accordingly
 cell_line_data = fread('../CCLE/CCLE_sample_info_file_2012-10-18.txt', header=T)
 drug_data = fread('../CCLE/CCLE_NP24.2009_Drug_data_2015.02.24.csv', header=T)
 gen_data = fread('CCLE_data_format_mutation.txt', header=T)
+ccle_genes = unlist(unique(gen_data[,1]))
+#Subset to genes existing in both training and test sets for validation
+overlap_genes = intersect(gene_list, ccle_genes)
+oinds = which(ccle_genes %in% overlap_genes)
+gen_data = gen_data[oinds,]
 
 #Split data and feature names
 gen_cell_data = gen_data[,3:ncol(gen_data)]
@@ -31,7 +38,12 @@ drug_data_cells = drug_data[dinds, ]
 #Use lambda with smallest MSE, determined via cross-validation
 #Conduct 10-fold cross validation to obtain R^2 for the predicted drug activity area vs. known
 #All files are written to current working directory
-all_r2s = c()
+all_r2s = matrix(c(rep(0,10*24)), nrow=24, ncol=10)
+rownames(all_r2s) = drugs
+#Train using cross-validation, or train on entire dataset
+cross = T
+overlap_drugs = c("Nilotinib", "Erlotinib", "PHA-665752", "Paclitaxel", "Sorafenib", "Lapatinib")
+model_list = list()
 for (drug in drugs) {
   dinds = which(drug_data_cells[,3] == drug)
   cell_labels = unlist(drug_data_cells[dinds,1])
@@ -44,34 +56,50 @@ for (drug in drugs) {
   #Normalize data via z-score
   gen_drug_data = scale(gen_data, center=TRUE, scale=TRUE)
   pred_num_data = apply(pred_data, 1, function(x) as.numeric(x))
-  flds = cvFolds(ncol(gen_drug_data), K=10)
-  tot_r2 = 0
-  all_coefs = gen_ft_data
   #For computational efficiency, only include features with r > 0.1 with outcome
   cors = apply(gen_drug_data, 1, function(x) cor(as.numeric(x), pred_num_data))
   kinds = which(cors > 0.1)
   gen_drug_data_keep = gen_drug_data[kinds,]
-  for (i in 1:10) {
-    train_inds <- flds$subsets[flds$which != i]
-    test_inds <- flds$subsets[flds$which == i]
-    pred_train = pred_num_data[train_inds]
-    gen_train = gen_drug_data_keep[,train_inds]
-    pred_test = pred_num_data[test_inds]
-    gen_test = gen_drug_data_keep[,test_inds]
-    fit = cv.glmnet((t(data.matrix(gen_train))), as.vector(pred_train), alpha=0.5)
-    betas = rep(0, nrow(gen_ft_data))
+  gen_ft_data_keep = gen_ft_data[kinds,]
+  if (cross == F) {
+    fit = cv.glmnet((t(data.matrix(gen_drug_data_keep))), as.vector(pred_num_data), alpha=0.5)
+    betas = rep(0, nrow(gen_ft_data_keep))
     coefs = coef(fit, s="lambda.min")
     betas[coefs@i] = coefs@x
-    all_coefs = cbind(all_coefs, betas)
-    pred = predict(fit, t(data.matrix(gen_test)), s="lambda.min")
-    r = cor(pred, pred_test, method='pearson')
-    r2 = paste("R^2= ", round(r^2,3), sep='')
-    r = cor(pred, pred_test, method='pearson')
-    tot_r2 = tot_r2 + r^2
+    fit_out = cbind(gen_ft_data_keep, betas)
+    out_file = paste(drug, '_betas.txt')
+    write.table(fit_out, file=out_file, sep='\t', row.names=F, col.names=F)
+    #Save models for validation step
+    model_list = append(model_list, list(fit))
   }
-  out_file = paste(drug, '_with_mutations_scale.txt', sep='')
-  write.table(all_coefs, out_file, sep='\t', row.names=F)
-  all_r2s = c(all_r2s, tot_r2/10)
+  else {
+    flds = cvFolds(ncol(gen_drug_data_keep), K=10)
+    all_coefs = gen_ft_data_keep
+    for (j in 1:10) {
+      tot_r2 = 0
+      for (i in 1:10) {
+        train_inds <- flds$subsets[flds$which != i]
+        test_inds <- flds$subsets[flds$which == i]
+        pred_train = pred_num_data[train_inds]
+        gen_train = gen_drug_data_keep[,train_inds]
+        pred_test = pred_num_data[test_inds]
+        gen_test = gen_drug_data_keep[,test_inds]
+        fit = cv.glmnet((t(data.matrix(gen_train))), as.vector(pred_train), alpha=0.5)
+        betas = rep(0, nrow(gen_ft_data_keep))
+        coefs = coef(fit, s="lambda.min")
+        betas[coefs@i] = coefs@x
+        all_coefs = cbind(all_coefs, betas)
+        pred = predict(fit, t(data.matrix(gen_test)), s="lambda.min")
+        r = cor(pred, pred_test, method='pearson')
+        r2_val = r^2
+        if (is.na(r2_val)) { r2_val = 0 }
+        tot_r2 = tot_r2 + r2_val
+      }
+      all_r2s[drug, j] = tot_r2/10
+    }
+    out_file = paste(drug, '_with_mutations_scale.txt', sep='')
+    write.table(all_coefs, out_file, sep='\t', row.names=F)
+  }
 }
 
 pred_outcomes = cbind(drugs, all_r2s)
